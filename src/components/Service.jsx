d14eec716a7e21460bc404bcf449d1247f23ae9e
Yash <utkarshkulshrestha2000@gmail.com>
2021-09-11 22:29:06 +0530

    Navbar logo

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "./Card";
+import list from "../data/listOfServices.json"
 
 function Service(props) {
   return (
@@ -8,80 +9,22 @@ function Service(props) {
         <h2 className="text-center mt-0">Our Services</h2>
         <hr className="divider" />
         <div className="row gx-4 gx-lg-5">
-        <div className="yash">
+          <div className="yash">
 
-          <div className="slider">
-            <div className="slide-track">
-              <div className="slide">
-                <Card title=" Agreement for Transfer of Flat" />
-              </div>
-              <div className="slide">
-                <Card
-                  title=" Devlopment / Re-Devlopment Agreement"
-
-                />
+            <div className="slider">
+              <div className="slide-track">
+                {list.items.map((item, i) => (
+                  <div className="slide">
+                    <Card title={item} />
+                  </div>
+                ))}
 
               </div>
-              <div className="slide">
-                <Card
-                  title="Package Deal Agreement"
-
-                />
-
-              </div>
-              <div className="slide">
-                <Card
-                  title="Leave & License Agreement"
-
-                />
-              </div>
-              <div className="slide">
-                <Card
-                  title="Joint Venture Agreement"
-
-                />
-              </div>
-              <div className="slide">
-                <Card
-                  title="Tripartie Agreement"
-
-                />
-              </div>
-              <div className="slide">
-                <Card
-                  title="Deed Of Admission"
-
-                />
-              </div>
-              <div className="slide">
-                <Card
-                  title="Society Registration"
-
-                />
-              </div>
-              <div className="slide">
-                <Card
-                  title="Company Registration"
-
-                />
-              </div>
-              <div className="slide">
-                <Card
-                  title="Death Certificate"
-                />
-              </div>
-              <div className="slide">
-                <Card
-                  title="E-Chalan"
-                />
-              </div>
-
             </div>
           </div>
         </div>
       </div>
-    </div>
-      </section >
+    </section >
 
   );
 }
